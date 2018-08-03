6ac758b348dfabcd3d51a0316d2df04782d3eca7
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-08-02 21:57:32 -0400

    Add /version

---
+++ commands/version.js
@@ -0,0 +1,10 @@
+const version = require("./../package.json").version;
+const verMsg = `I am running version ${version}! 😄`;
+
+module.exports = {
+    command: "version",
+    describe: "Shows the version of the bot.",
+    handler: args => {
+        args.message.send(verMsg);
+    }
+};
