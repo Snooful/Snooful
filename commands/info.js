7b1874a1bc52a640bbab82746777a24378da0d68
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-08-02 20:52:52 -0400

    Adjust prefix in info message

--- commands/info.js
+++ commands/info.js
@@ -3,9 +3,9 @@ module.exports = {
     describe: "Shows info about this bot.",
     handler: args => {
         args.message.send([
-            `I am ${args.client.username}, ready to help you with anything you need!`,
-            "My prefix is /, so to type a command you just do /command.",
-            "I was made by haykam821, but please direct all questions and concerns via /support!"
+            `I am u/${args.client.username}, ready to help you with anything you need!`,
+            `My prefix is ${args.prefix}, so to type a command you just do ${args.prefix}command.`,
+            `I was made by haykam821, but please direct all questions and concerns via ${args.prefix}support!`,
         ].join("\n"));
     }
 };
