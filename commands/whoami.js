bcb77ec590e473e7a925e42498f49a798774f594
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-08-16 15:34:10 -0400

    Add whoami command

---
+++ commands/whoami.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: "whoami",
+	describe: "Tells you who you are.",
+	handler: args => {
+		args.send(`Ah, silly humans, always forgetting their names. I won't throw you under the shade too much this time, though, u/${args.message._sender.nickname}.`);
+	}
+};
