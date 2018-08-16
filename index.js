8e5b4585d2b3b39cbb153590f34ef0adadd37708
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-08-16 16:35:18 -0400

    Mention inviter in introductory message

--- index.js
+++ index.js
@@ -80,11 +80,11 @@ sb.connect(process.env["SNOOFUL_ID"], process.env["SNOOFUL_TOKEN"], (userInfo, e
 const handler = new sb.ChannelHandler();
 
 handler.onMessageReceived = (channel, message) => handleCommand(message.message, channel, message);
-handler.onUserReceivedInvitation = channel => {
+handler.onUserReceivedInvitation = (channel, inviter) => {
 	log.events("invited to channel");
 	channel.join(() => {
 		log.events("automatically joined channel via invitation");
-		channel.sendUserMessage(`Thanks for letting me into the channnel! I'm u/${client.nickname}, your friendly bot asssistant.`, () => {
+		channel.sendUserMessage(`Thanks for letting me into the channnel, u/${inviter.nickname}! I'm u/${client.nickname}, your friendly bot asssistant.`, () => {
 			log.events("sent introductory message");
 		});
 	});
