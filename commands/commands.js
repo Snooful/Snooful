b1ce62717ed1f4209ebba16fe0b7e63a4daff227
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-08-16 14:49:59 -0400

    Fix formatting of !commands

--- commands/commands.js
+++ commands/commands.js
@@ -1,5 +1,8 @@
 module.exports = {
     command: "commands",
     describe: "List all commands.",
-    handler: args => args.send("Commands: " + args.usage.join(", ")),
+    handler: args => {
+		const list = args.usage.map(command => `${args.prefix}${command[0]}: ${command[1]}`);
+		args.send("Commands:\n- " + list.join("\n- "));
+	},
 };
