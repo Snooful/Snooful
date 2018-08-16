e8ae5500ba4de727e23da06582c0fc1859e68921
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-08-16 17:54:44 -0400

    Add echo command

---
+++ commands/echo.js
@@ -0,0 +1,11 @@
+module.exports = {
+	command: "echo <text...>",
+	describe: "Repeats a message.",
+	builder: cmd => {
+		cmd.positional("text", {
+			type: "string",
+			describe: "The message to echo."
+		})
+	},
+	handler: args => args.send("\u200B" + args.text.join(" ")),
+};
