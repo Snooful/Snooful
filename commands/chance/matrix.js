dacdd163167d733ae73f0c8841e5771b7eb2bfb6
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-08-16 14:55:08 -0400

    Make matrix longer

--- commands/chance/matrix.js
+++ commands/chance/matrix.js
@@ -3,7 +3,7 @@ const chance = require("chance").Chance();
 module.exports = {
     command: "matrix",
     describe: "Provides a line of random binary.",
-    handler: args => args.send(chance.n(chance.integer, 16, {
+    handler: args => args.send(chance.n(chance.integer, 40, {
     	min: 0,
     	max: 1,
     }).join("")),
