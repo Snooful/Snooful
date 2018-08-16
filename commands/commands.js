f607c733cf2e1943c90ed3e12c09e82803c66c8f
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-08-16 14:50:16 -0400

    Adjust description for !commands

--- commands/commands.js
+++ commands/commands.js
@@ -1,6 +1,6 @@
 module.exports = {
     command: "commands",
-    describe: "List all commands.",
+    describe: "List all commands and their description.",
     handler: args => {
 		const list = args.usage.map(command => `${args.prefix}${command[0]}: ${command[1]}`);
 		args.send("Commands:\n- " + list.join("\n- "));
