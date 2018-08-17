52763c6306853d5b258eaea43ca879a62b2c9609
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-08-17 05:22:05 -0400

    Add landing on its side to possible outputs of a coin flip

--- commands/chance/flip.js
+++ commands/chance/flip.js
@@ -4,5 +4,13 @@ module.exports = {
 	command: "flip",
 	alias: "coin",
     describe: "Flips a coin.",
-    handler: args => args.send(`The coin landed on ${chance.coin()}!`),
+    handler: args => args.send(`The coin landed on ${chance.weighted([
+		"heads",
+		"tails",
+		"its side",
+	], [
+		25,
+		25,
+		1,
+	])}!`),
 };
