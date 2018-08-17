8df59d2d36367b0900e8da26838bc54aa61152fd
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-08-17 16:11:11 -0400

    Paginate commands list

--- commands/commands.js
+++ commands/commands.js
@@ -1,8 +1,23 @@
+const chunk = require("lodash.chunk");
+
 module.exports = {
-    command: "commands",
-    describe: "List all commands and their description.",
+    command: "commands <page>",
+	describe: "List commands and their description.",
+	builder: builder => {
+		builder.positional("page", {
+			type: "number",
+			describe: "The page number to view.",
+			default: 1,
+		});
+	},
     handler: args => {
-		const list = args.usage.map(command => `${args.prefix}${command[0]}: ${command[1]}`).sort();
-		args.send(`Commands (${list.length}): \n- ` + list.join("\n- "));
+		const allCommands = args.usage.map(command => `${args.prefix}${command[0]}: ${command[1]}`).sort();
+		const list = chunk(allCommands, 5);
+
+		if (args.page <= list.length && args.page > 0) {
+			args.send(`${allCommands.length} commands (page ${args.page} of ${list.length}): \n\n- ` + list[args.page - 1].join("\n- "));
+		} else {
+			args.send("That's an invalid page number!");
+		}
 	},
 };
