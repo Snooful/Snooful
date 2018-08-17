a0db69b183aa07c3a4a0bf3a52d6a70991b78096
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-08-16 20:00:15 -0400

    Add number of commands to the commands command
    That's a mouthful.

--- commands/commands.js
+++ commands/commands.js
@@ -3,6 +3,6 @@ module.exports = {
     describe: "List all commands and their description.",
     handler: args => {
 		const list = args.usage.map(command => `${args.prefix}${command[0]}: ${command[1]}`);
-		args.send("Commands:\n- " + list.join("\n- "));
+		args.send(`Commands (${list.length}): \n- ` + list.join("\n- "));
 	},
 };
