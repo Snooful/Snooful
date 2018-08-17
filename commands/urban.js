ffa1b17909db32f934d561126ac26375bb84eb54
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-08-17 02:37:36 -0400

    Rewrite urban command to use then and catch

--- commands/urban.js
+++ commands/urban.js
@@ -3,22 +3,20 @@ const urbanDict = require("urban-dictionary");
 module.exports = {
 	command: "urban <term>",
 	describe: "Gets a term from Urban Dictionary.",
-	handler: async args => {
+	handler: args => {
 		if (args.term) {
-			const result = await urbanDict.term(args.term);
-			const entry = result.entries[0];
-
-			if (entry) {
+			urbanDict.term(args.term).then(result => {
+				const entry = result.entries[0];
 				args.send([
 					`${entry.word} (${entry.thumbs_up} votes)`,
 					"",
 					entry.definition,
 				].join("\n"));
-			} else {
+			}).catch(() => {
 				args.send("There was no result for that word on Urban Dictionary!");
-			}
+			});
 		} else {
-			args.send(`undefined - A subtle hint that you need to tell me which word to define.`);
+			args.send(`undefined: A subtle hint that you need to tell me which word to define.`);
 		}
 	}
 };
