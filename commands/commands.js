a778d644bdf440eadc28437c7c3b2096ebf1d25b
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-08-17 16:05:18 -0400

    Sort commands list

--- commands/commands.js
+++ commands/commands.js
@@ -2,7 +2,7 @@ module.exports = {
     command: "commands",
     describe: "List all commands and their description.",
     handler: args => {
-		const list = args.usage.map(command => `${args.prefix}${command[0]}: ${command[1]}`);
+		const list = args.usage.map(command => `${args.prefix}${command[0]}: ${command[1]}`).sort();
 		args.send(`Commands (${list.length}): \n- ` + list.join("\n- "));
 	},
 };
