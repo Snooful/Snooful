5ade3140b70236d38ca5a8bb968765cda2c5d5ab
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-08-19 23:02:32 -0400

    Add last commit’s short hash to version command

--- commands/version.js
+++ commands/version.js
@@ -1,5 +1,12 @@
+const gitLatestHash = require("git-latest-hash");
+
 module.exports = {
     command: "version",
     describe: "Shows the version of the bot.",
-    handler: args => args.send(`I am running version ${args.version}! 😄`),
+    handler: args => gitLatestHash.getLastCommit((error, git) => {
+		if (error) {
+			args.send(`I am running version ${args.version}! 😄`);
+		} else {
+			args.send(`I am running version ${args.version}, with the most recent commit being ${git.shortHash}!`);
+	}),
 };
