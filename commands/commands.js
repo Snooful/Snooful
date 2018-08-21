73ef88ef1167ac7159000b1a5c238a5dcc97f233
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-08-21 14:34:48 -0400

    Use pagination utility for commands command

--- commands/commands.js
+++ commands/commands.js
@@ -1,23 +1,2 @@
-const chunk = require("lodash.chunk");
-
-module.exports = {
-    command: "commands [page]",
-	describe: "List commands and their description.",
-	builder: builder => {
-		builder.positional("page", {
-			type: "number",
-			describe: "The page number to view.",
-			default: 1,
-		});
-	},
-    handler: args => {
-		const allCommands = args.usage.map(command => `${args.prefix}${command[0]}: ${command[1]}`).sort();
-		const list = chunk(allCommands, 5);
-
-		if (args.page <= list.length && args.page > 0) {
-			args.send(`${allCommands.length} commands (page ${args.page} of ${list.length}): \n\n• ` + list[args.page - 1].join("\n• "));
-		} else {
-			args.send("That's an invalid page number!");
-		}
-	},
-};
+const paginate = require("./../utils/paginate.js");
+module.exports = paginate("commands", "List commands and their description.", args.usage.map(command => `${args.prefix}${command[0]}: ${command[1]}`), "commands");
