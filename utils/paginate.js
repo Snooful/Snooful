3abf2289cc1f2cc629adad535900983340db1f12
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-08-21 14:34:28 -0400

    Ability to use function for pagination data

--- utils/paginate.js
+++ utils/paginate.js
@@ -4,10 +4,10 @@ const chunk = require("lodash.chunk");
 	* Creates a pagined command using a set of data.
 	* @param {string} command The name of the command.
 	* @param {string} description The command description.
-	* @param {*[]} data The data to paginate.
+	* @param {function|*[]} data The data to paginate.
 	* @param {string} dataType The type of data being displayed.
 */
-module.exports = (command, description, data = [], dataType = "items") => {
+module.exports = async (command, description, data = [], dataType = "items") => {
     command: command + " [page]",
 	describe: description,
 	builder: builder => {
@@ -18,10 +18,12 @@ module.exports = (command, description, data = [], dataType = "items") => {
 		});
 	},
     handler: args => {
-		const list = chunk(data.sort(), 5);
+	    	const resolvedData = typeof data === "function" ? await data(args) : data;
+	    
+		const list = chunk(resolvedData.sort(), 5);
 
 		if (args.page <= list.length && args.page > 0) {
-			args.send(`${data.length} commands (page ${args.page} of ${list.length}): \n\n• ` + list[args.page - 1].join("\n• "));
+			args.send(`${resolvedData.length} commands (page ${args.page} of ${list.length}): \n\n• ` + list[args.page - 1].join("\n• "));
 		} else {
 			args.send("That's an invalid page number!");
 		}
