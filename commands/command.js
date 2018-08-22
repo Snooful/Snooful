89749ebb16ce12f50e76c6fac8ffc6a0282e3fd7
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-08-22 18:52:51 -0400

    Implement command lookup command

--- commands/command.js
+++ commands/command.js
@@ -16,7 +16,16 @@ module.exports = {
 	},
 	handler: args => {
 		if (args.name) {
-			
+			const command = args.usage.filter(usage => usage[0].split(" ")[0] === args.name)[0];
+			if (command) {
+				args.send([
+					"Command: " + args.prefix + command[0],
+					command[1] ? "Description: " + command[1] : "",
+					command[3].length > 0 ? "Aliases: " + command[3].join(", ") : "",
+				].join("\n"));
+			} else {
+				args.send("There is no command with that name.");
+			}
 		} else {
 			args.send(`Please specify a command to view. To see all commands, do ${args.prefix}commands.`);
 		}
