6e569cd42a93f61c36ff1847e746cc4f72a18e13
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-08-23 11:43:39 -0400

    Add aliases for github command

--- commands/github.js
+++ commands/github.js
@@ -3,6 +3,13 @@ const github = "https://github.com/Snooful/Snooful";
 module.exports = {
 	name: "github",
 	describe: "Links to the GitHub repository for Snooful.",
+	aliases: [
+		"repo",
+		"repository",
+		"readme",
+		"homepage",
+		"website",
+	],
 	handler: args => {
 		args.send(`My source code is available at ${github}!`);
 	}
