88b73701b605548c1292334325cec059aec0a0bb
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-08-26 10:27:14 -0400

    Update parameters in commands' usage of pagination utility

--- commands/commands.js
+++ commands/commands.js
@@ -1,4 +1,7 @@
 const paginate = require("./../utils/paginate.js");
-module.exports = paginate("commands", "List commands and their description.", args => {
+module.exports = paginate("commands", args => {
 	return args.usage.map(command => `${args.prefix}${command[0]}: ${command[1]}`);
-}, "commands");
+}, {
+	description: "List commands and their description.",
+	dataType: "commands",
+});
