348078233bb5739907f847f914c4671bc8c7070e
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-09-01 11:11:49 -0400

    Enforce min and max count in namecolor command

--- commands/colors/namecolor.js
+++ commands/colors/namecolor.js
@@ -25,7 +25,13 @@ module.exports = {
 				if (args.count === 1) {
 					args.send(`This color is called ${names[0]}.`);
 				} else if (Number.isInteger(args.count)) {
-					args.send(`The closest names for this color are ${names.slice(0, args.count).join(", ")}.`);
+					if (args.count > 10) {
+						args.send(`I cannot provide you with ${args.count} colors because that would spam this chat.`);
+					} else if (args.count < 1) {
+						args.send("I need to provide you with at least one name for this color.");
+					} else {
+						args.send(`The closest names for this color are ${names.slice(0, args.count).join(", ")}.`);
+					}
 				} else {
 					args.send("Please provide a proper integer for the amount of color names you would like to me to give.");
 				}
