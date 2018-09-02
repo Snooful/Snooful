18fd308a3430845ab1c850ae6b382a3781951bf5
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-09-02 15:08:02 -0400

    Allow non-exact choices in rps command

--- commands/chance/rps.js
+++ commands/chance/rps.js
@@ -1,11 +1,11 @@
 const chance = require("chance").Chance();
+const dym = require("didyoumean2");
 
 const choices = [
 	"rock",
 	"paper",
 	"scissors",
-]
-
+];
 const beats = {
 	"rock": "scissors",
 	"scissors": "paper",
@@ -17,19 +17,23 @@ module.exports = {
 	describe: "Plays rock-paper-scissors with the bot.",
 	handler: args => {
 		if (args.option) {
-			if (choices.includes(args.option)) {
+			const yourChoice = dym(args.option, choices, {
+				threshold: 0.7,
+			});
+
+			if (yourChoice === null) {
+				args.send(`You must choose a valid move! Your choices are ${choices.join(", ")}.`);
+			} else {
 				const msg = "Rock... paper... scissors...\n\n";
 				const myChoice = chance.pickone(choices);
 
-				if (myChoice === args.option) { 
+				if (myChoice === yourChoice) {
 					args.send(msg + `Oh, we both chose ${myChoice}! We should try this again.`);
 				} else if (beats[myChoice] === args.option) {
-					args.send(msg + `Haha, I beat you; ${myChoice} beats ${args.option}.`);
+					args.send(msg + `Haha, I beat you; ${myChoice} beats ${yourChoice}.`);
 				} else {
-					args.send(msg + `Darn, you got me there; ${args.option} beats ${myChoice}.`);
+					args.send(msg + `Darn, you got me there; ${yourChoice} beats ${myChoice}.`);
 				}
-			} else {
-				args.send(`You must choose a valid move! Your choices are ${choices.join(", ")}.`);
 			}
 		} else {
 			args.send("You need to specify the move you would like to make.");
