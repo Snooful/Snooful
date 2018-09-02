3f8e140313dbd2af84dc319942bd7c330cf5d82a
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-09-02 14:44:46 -0400

    Add sum to customroll command

--- commands/chance/customroll.js
+++ commands/chance/customroll.js
@@ -15,7 +15,10 @@ module.exports = {
 			if (roll.rolls.length === 0) {
 				args.send("That seems to be invalid syntax. Please use proper dice notation.");
 			} else {
-				args.send(`Dice output: ${roll.rolls[0].join(", ")}.`);
+				const lands = roll.rolls[0];
+				const sum = lands.reduce((prev, curr) => prev + curr);
+
+				args.send(`Dice output: ${lands.join(", ")}, with a total of ${sum}.`);
 			}
 		} else {
 			args.send("Specify the dice you want to roll using standard notation.");
