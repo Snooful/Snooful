6c17e98997ff9a41d9495e1c56a2b932ae78500b
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-09-05 18:23:49 -0400

    Make git-last-commit optional

--- commands/version.js
+++ commands/version.js
@@ -1,13 +1,29 @@
-const glc = require("git-last-commit");
+let glc, canUseHash;
+try {
+	glc = require("git-last-commit");
+	canUseHash = true;
+} catch (_) {
+	canUseHash = false;
+}
+
+function sendVersion(args) {
+	return args.send(`I am running version ${args.version}! 😄`);
+}
 
 module.exports = {
     command: "version",
     describe: "Shows the version of the bot.",
-    handler: args => glc.getLastCommit((error, git) => {
-		if (error) {
-			args.send(`I am running version ${args.version}! 😄`);
+    handler: args => {
+		if (canUseHash) {
+			glc.getLastCommit((error, git) => {
+				if (error) {
+					sendVersion(args);
+				} else {
+					args.send(`I am running version ${args.version}, with the most recent commit being ${git.shortHash}!`);
+				}
+			});
 		} else {
-			args.send(`I am running version ${args.version}, with the most recent commit being ${git.shortHash}!`);
+			sendVersion(args);
 		}
-	}),
+	},
 };
