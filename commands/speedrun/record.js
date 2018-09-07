4a48dda76d8bda28d0a11fcee93c97ea9a7b3f8e
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-09-07 18:53:00 -0400

    Use got in record command

--- commands/speedrun/record.js
+++ commands/speedrun/record.js
@@ -1,5 +1,5 @@
 const url = require("url-escape-tag");
-const rp = require("request-promise-native");
+const got = require("got");
 
 const moment = require("moment");
 require("moment-duration-format");
@@ -14,14 +14,19 @@ module.exports = {
 	describe: "Gets the world record for a game on speedrun.com.",
 	handler: args => {
 		if (args.game) {
-			rp(url`https://www.speedrun.com/api/v1/games?name=${args.game}&max=1`).then(gamesRaw => {
-				const game = JSON.parse(gamesRaw).data[0];
-				rp(url `https://www.speedrun.com/api/v1/games/${game.id}/records?miscellaneous=no&scope=full-game&top=1`).then(runsRaw => {
-					const topRun = JSON.parse(runsRaw).data[0].runs[0].run;
+			got(url`https://www.speedrun.com/api/v1/games?name=${args.game}&max=1`, {
+				json: true,
+			}).then(gameResponse => {
+				const game = gameResponse.body.data[0];
+				got(url `https://www.speedrun.com/api/v1/games/${game.id}/records?miscellaneous=no&scope=full-game&top=1`, {
+					json: true,
+				}).then(runsResponse => {
+					const topRun = runsResponse.body.data[0].runs[0].run;
 					const speed = moment.duration(topRun.times.primary_t, "seconds").format("h [hours], m [minutes], s [seconds]");
 					args.send(`The world record for ${game.names.international} is at ${speed}. For more information, view ${topRun.weblink}.`);
 				});
-			}).catch(() => {
+			}).catch(err => {
+				console.log(err);
 				args.send("I could not fetch the game!");
 			});
 		} else {
