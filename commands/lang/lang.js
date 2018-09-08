36a1c351b04afcc4d1befe064d8b647eb17282d6
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-09-08 16:40:39 -0400

    Add lang command

---
+++ commands/lang/lang.js
@@ -0,0 +1,5 @@
+module.exports = {
+	command: "lang",
+	describe: "Gets the current language of Snooful.",
+	handler: args => args.send(args.localize("current_language", args.localize("language"), args.prefix)),
+};
