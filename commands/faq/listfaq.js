0d83058c8bcb076a30a488f84371eb39228291d8
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-09-09 13:18:23 -0400

    Completely localize pagination

--- commands/faq/listfaq.js
+++ commands/faq/listfaq.js
@@ -1,8 +1,10 @@
 const paginate = require("./../../utils/paginate.js");
+
 module.exports = paginate("listfaq", args => {
 	const messages = args.settings.get("faq_messages") || {};
 	return Object.keys(messages).map(key => `${key} (${messages[key].length} characters long)`);
 }, {
-	dataType: "FAQs",
+	dataType: "faq_datatype",
 	description: "Lists all the FAQs.",
+	noItemsMessage: "no_faqs",
 });
