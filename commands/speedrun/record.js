8c1e6800d14f3349e72dba7a82c115d455c6370a
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-09-12 14:35:16 -0400

    Combine options objects for fetch utility in record command

--- commands/speedrun/record.js
+++ commands/speedrun/record.js
@@ -14,18 +14,20 @@ module.exports = {
 	describe: "Gets the world record for a game on record.com.",
 	handler: args => {
 		if (args.game) {
-			fetch(url`https://www.speedrun.com/api/v1/games?name=${args.game}&max=1`, {
-				json: true,
-			}, args, {
+			fetch(url`https://www.speedrun.com/api/v1/games?name=${args.game}&max=1`, args, {
 				contentType: "record_game_type",
 				errorKeyPrefix: "record",
+				got: {
+					json: true,
+				},
 			}).then(gameResponse => {
 				const game = gameResponse.body.data[0];
-				fetch(url`https://www.speedrun.com/api/v1/games/${game.id}/records?miscellaneous=no&scope=full-game&top=1`, {
-					json: true,
-				}, args, {
+				fetch(url`https://www.speedrun.com/api/v1/games/${game.id}/records?miscellaneous=no&scope=full-game&top=1`, args, {
 					contentType: "record_runs_type",
 					errorKeyPrefix: "record",
+					got: {
+						json: true,
+					},
 				}).then(runsResponse => {
 					const topRun = runsResponse.body.data[0].runs[0].run;
 					const speed = moment.duration(topRun.times.primary_t, "seconds").format("h [hours], m [minutes], s [seconds]");
