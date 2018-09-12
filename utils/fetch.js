63978cec7bab2459a0f350ad2690d56168724096
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-09-12 14:42:06 -0400

    Fallback to a default error message if its custom-prefixed variant is missing

--- utils/fetch.js
+++ utils/fetch.js
@@ -1,32 +1,41 @@
 const got = require("got");
 
+function safeLocal(localize, prefix, otherPart, ...formats) {
+	const customed = localize(prefix + "_" + otherPart, ...formats);
+	if (customed) {
+		return customed;
+	} else {
+		return localize(`fetch_${otherPart}`, ...formats);
+	}
+}
+
 function errorHandler(error, send, localize, type = "game", prefix = "fetch") {
 	if (error instanceof got.CacheError) {
-		send(localize(prefix + "_cache_error", type));
+		send(safeLocal(localize, prefix, "cache_error", type));
 	} else if (error instanceof got.RequestError) {
-		send(localize(prefix + "_request_error", type));
+		send(safeLocal(localize, prefix, "request_error", type));
 	} else if (error instanceof got.ReadError) {
-		send(localize(prefix + "_read_error", type));
+		send(safeLocal(localize, prefix, "read_error", type));
 	} else if (error instanceof got.ParseError) {
-		send(localize(prefix + "_parse_error", type));
+		send(safeLocal(localize, prefix, "parse_error", type));
 	} else if (error instanceof got.HTTPError) {
 		if (error.statusCode.startsWith("4")) {
-			send(localize(prefix + "_4xx_error", type, error.statusCode));
+			send(safeLocal(localize, prefix, "4xx_error", type, error.statusCode));
 		} else if (error.statusCode.startsWith("5")) {
-			send(localize(prefix + "_5xx_error", type, error.statusCode));
+			send(safeLocal(localize, prefix, "5xx_error", type, error.statusCode));
 		} else {
-			send(localize(prefix + "_http_error", type, error.statusCode));
+			send(safeLocal(localize, prefix, "http_error", type, error.statusCode));
 		}
 	} else if (error instanceof got.MaxRedirectsError) {
-		send(localize(prefix + "_redirect_error", type));
+		send(safeLocal(localize, prefix, "redirect_error", type));
 	} else if (error instanceof got.UnsupportedProtocolError) {
-		send(localize(prefix + "_protocol_error", type));
+		send(safeLocal(localize, prefix, "protocol_error", type));
 	} else if (error instanceof got.CancelError) {
-		send(localize(prefix + "_cancel_error", type));
+		send(safeLocal(localize, prefix, "cancel_error", type));
 	} else if (error instanceof got.TimeoutError) {
-		send(localize(prefix + "_timeout_error", type));
+		send(safeLocal(localize, prefix, "timeout_error", type));
 	} else {
-		send(localize(prefix + "_generic_error", type));
+		send(safeLocal(localize, prefix, "generic_error", type));
 	}
 }
 
