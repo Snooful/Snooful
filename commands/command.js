951a94460b639b06c3348960f608267871c75cde
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-10-23 16:23:10 -0400

    Use bullets instead of dashes for lists in command info

--- commands/command.js
+++ commands/command.js
@@ -18,30 +18,34 @@ module.exports = {
 			const command = args.usage.filter(usage => usage[0].split(" ")[0] === args.name)[0];
 			if (command) {
 				const msg = [];
-				
+
+				// Full command template
 				msg.push(args.prefix + command.toString());
-				
+
+				// Description
 				if (command.description) {
 					msg.push("Description: " + command.description);
 				}
-				
-				
+
+				// Aliases
 				if (command.aliases.length > 0) {
 					if (command.aliases.length === 1) {
 						msg.push("Alias: " + args.prefix + command.aliases[0]);
 					} else {
-						msg.push("Aliases:\n  - " + args.prefix + command.aliases.join("\n  - " + args.prefix));
+						msg.push("Aliases:\n•" + args.prefix + command.aliases.join("\n•" + args.prefix));
 					}
 				}
-				
-				if (command.arguments.length > 0) {			
+
+				// Arguments
+				if (command.arguments.length > 0) {
 					const arglist = command.arguments.map(arg => {
 						const argtype = args.localize("argument_type" + arg.type);
-						return `- ${arg.name} (${argtype}): ${arg.description}`;
+						return `• ${arg.name} (${argtype}): ${arg.description}`;
 					});
-					msg.push("Arguments:\n  " + arglist.join("\n  "));
+					msg.push("Arguments:\n" + arglist.join("\n"));
 				}
-					
+
+				// Send it!
 				args.send(msg.join("\n\n"));
 			} else {
 				args.send(args.localize("command_not_found"));
