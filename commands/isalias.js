777054734a82ad5362eafc8e57c1be23a0be8e0c
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-10-24 18:16:17 -0400

    Sort keys in isalias.js

--- commands/isalias.js
+++ commands/isalias.js
@@ -5,7 +5,6 @@ module.exports = {
 		type: "string",
 	}],
 	description: "Checks if a given command is an alias.",
-	longDescription: "Checks if the given command is an alias of another. If so, gives the name of the command it is an alias of.",
 	handler: args => {
 		if (args.command) {
 			const cmd = args.registry.filter(command => command.name === args.command)[0];
@@ -22,5 +21,6 @@ module.exports = {
 			args.send(args.localize("isalias_unspecified_command"));
 		}
 	},
+	longDescription: "Checks if the given command is an alias of another. If so, gives the name of the command it is an alias of.",
 	name: "isalias",
 };
