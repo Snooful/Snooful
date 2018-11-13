ae424855a1503fb26c7d6396ecb287d293cb6236
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-11-13 12:18:31 -0500

    Document the promisify utility

--- utils/promisify.js
+++ utils/promisify.js
@@ -1,4 +1,11 @@
 const pify = require("pify");
+
+/**
+ * Promisifies then runs a function.
+ * @param {Function} The function to promisify and run.
+ * @param {*[]} The arguments to pass to the function when running it.
+ * @returns {Promise}
+ */
 module.exports = (input, ...args) => {
 	return pify(input, {
 		errorFirst: true,
