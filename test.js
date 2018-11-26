165ba7237624faf6985fcac1b487d735311d5301
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-11-26 12:06:36 -0500

    Test locales for sorted keys

--- test.js
+++ test.js
@@ -5,16 +5,26 @@ const assert = require("chai").assert;
 const locales = require("./src/locales.json");
 const validate = require("locale-code").validate;
 
+const sorted = require("is-sorted");
+function alphaSorted(array = []) {
+	return sorted(array, (a, b) => {
+		return a.localeCompare(b);
+	}); 
+}
+
 describe("localizations", () => {
 	Object.keys(locales).forEach(key => {
 		const locale = locales[key];
-		describe(`${locale.language} (${key}) locale`, () => {
+		describe(`${key} locale`, () => {
 			it("has a correct locale code", () => {
 				assert.isTrue(validate(key));
 			});
 			it("is a non-empty object", () => {
 				assert.isObject(locale);
 				assert.notDeepEqual(locale, {});
+			});
+			it("has alphabetically-sorted keys", () => {
+				assert.isTrue(alphaSorted(Object.keys(locale)));
 			});
 		});
 	});
