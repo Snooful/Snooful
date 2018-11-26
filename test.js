d609cd61ba47267cbc4300971f8dbad5797b9009
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-11-26 12:22:00 -0500

    Write tests for localization value formats

--- test.js
+++ test.js
@@ -26,6 +26,53 @@ describe("localizations", () => {
 			it("has alphabetically-sorted keys", () => {
 				assert.isTrue(alphaSorted(Object.keys(locale)));
 			});
+			
+			describe("localization formats", () => {
+				// Our main values
+				const values = Object.values(locale);
+				
+				// Now for each type
+				const arrValues = [];
+				const objValues = [];
+				const otrValues = [];
+				
+				// Now fill our per-type value arrays
+				values.forEach(value => {
+					if (Array.isArray(value)) {
+						arrValues.push(value);
+					} else if (value.constructor === Object && !!value) {
+						objValues.push(value);
+					} else {
+						otrValues.push(value);
+					}
+				});
+				
+				// Now we can test these
+				it("arrays contain only strings", () => {
+					arrValues.every(arrValue => {
+						return arrValue.every(arrValueElem => {
+							return typeof arrValueElem === "string";
+						});
+					});
+				});
+				describe("object values", () => {
+					it("keys are integers", () => {
+						Object.keys(objValues).every(intMaybe => {
+							return Number.isInteger(intMaybe);
+						});
+					});
+					it("values are strings", () => {
+						Object.values(objValues).every(strMaybe => {
+							return typeof strMaybe === "string";
+						});
+					});
+				});
+				it("other values are strings", () => {
+					otrValues.every(otrValue => {
+						return typeof otrValue === "string";
+					});
+				});
+			});
 		});
 	});
 });
