ce2049c22c1a88b3c1a785d2c35f25c32b27df91
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-12-07 12:21:31 -0500

    Use eventMessageFactory utility for join and leave messages

--- src/index.js
+++ src/index.js
@@ -269,46 +269,8 @@ handler.onUserReceivedInvitation = (channel, inviter, invitees) => {
 	}
 };
 
-/**
- * Gets the subreddit from a channel.
- * @param {*} channel The channel to get the subreddit from.
-*/
-function channelSub(channel) {
-	if (channel.data) {
-		const data = JSON.parse(channel.data);
-		return data.subreddit ? data.subreddit.name : channel.url;
-	} else {
-		return channel.url;
-	}
-}
-
-handler.onUserJoined = (channel, user) => {
-	if (user.nickname === client.nickname) return;
-
-	log.gateway("user joined, handling join message");
-
-	const sub = channelSub(channel);
-	if (settings.get(sub, "join_message") !== undefined) {
-		pify(channel.sendUserMessage.bind(channel), settings.get(sub, "join_message").replace(/{USER}/g, user.nickname)).then(() => {
-			log.gateway("sent join message");
-		}).catch(() => {
-			log.gateway("failed to send join message");
-		});
-	}
-};
-handler.onUserLeft = (channel, user) => {
-	if (user.nickname === client.nickname) return;
-
-	log.gateway("user left, handling leave message");
-
-	const sub = channelSub(channel);
-	if (settings.get(sub, "leave_message") !== undefined) {
-		pify(channel.sendUserMessage.bind(channel), settings.get(sub, "leave_message").replace(/{USER}/g, user.nickname)).then(() => {
-			log.gateway("sent leave message");
-		}).catch(() => {
-			log.gateway("failed to send leave message");
-		});
-	}
-};
+const eventMessageFactory = require("./utils/event-message-factory.js");
+handler.onUserJoined = eventMessageFactory("join");
+handler.onUserLeft = eventMessageFactory("leave");
 
 sb.addChannelHandler("handler", handler);
