8dd3559c011da9f855522d409c5c61705e2a71fe
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-12-07 12:19:14 -0500

    Create event-message-factory.js

---
+++ src/utils/event-message-factory.js
@@ -0,0 +1,17 @@
+function eventMessageFactory(type, handleSelf = false) {
+	return (channel, user) => {
+		if (user.nickname === client.nickname && !handleSelf) return;
+
+		log.gateway("handling %s message", type);
+
+		const sub = channelSub(channel);
+		if (settings.get(sub, type + "_message") !== undefined) {
+			pify(channel.sendUserMessage.bind(channel), settings.get(sub, type + "_message").replace(/{USER}/g, user.nickname)).then(() => {
+				log.gateway("sent %s message", type);
+			}).catch(() => {
+				log.gateway("failed to send %s message", type);
+			});
+		}
+	};
+}
+module.exports = eventMessageFactory;
