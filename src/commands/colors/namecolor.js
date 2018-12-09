1d4ac05f52574c894d2621bf091771ae2281c1d3
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-12-09 10:15:22 -0500

    Use a color argument in the namecolor command

--- src/commands/colors/namecolor.js
+++ src/commands/colors/namecolor.js
@@ -8,7 +8,8 @@ module.exports = {
 	arguments: [{
 		description: "The color to name.",
 		key: "color",
-		type: "string",
+		required: true,
+		type: "color",
 	}, {
 		default: 1,
 		description: "The amount of names to assign.",
@@ -17,30 +18,15 @@ module.exports = {
 	}],
 	describe: "Names a color.",
 	handler: args => {
-		if (args.color) {
-			try {
-				const names = namer(args.color).ntc.map(result => result.name);
-				if (args.count === 1) {
-					args.send(args.localize("color_name", names[0]));
-				} else if (Number.isInteger(args.count)) {
-					if (args.count > 10) {
-						args.send(args.localize("color_amount_too_high", args.count));
-					} else if (args.count < 1) {
-						args.send(args.localize("color_amount_too_low"));
-					} else {
-						args.send(args.localize("color_names", names.slice(0, args.count).join(", ")));
-					}
-				} else {
-					args.send(args.localize("color_amount_invalid"));
-				}
-			} catch (error) {
-				args.send(args.localize("color_invalid", chance.color({
-					casing: "upper",
-					format: "hex",
-				})));
-			}
+		const names = namer(args.color.hex()).ntc.map(result => result.name);
+		if (args.count === 1) {
+			args.send(args.localize("color_name", names[0]));
+		} else if (args.count > 10) {
+			args.send(args.localize("color_amount_too_high", args.count));
+		} else if (args.count < 1) {
+			args.send(args.localize("color_amount_too_low"));
 		} else {
-			args.send(args.localize("color_unspecified"));
+			args.send(args.localize("color_names", names.slice(0, args.count).join(", ")));
 		}
 	},
 	name: "namecolor",
