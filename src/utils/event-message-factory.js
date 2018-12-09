78090d8edd3cff4f05690194a1f4ead6c48b3af5
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-12-08 20:08:37 -0500

    Document event message factory utility

--- src/utils/event-message-factory.js
+++ src/utils/event-message-factory.js
@@ -1,7 +1,16 @@
 const channelSub = require("./channel-sub.js");
 const pify = require("./promisify.js");
 
-function eventMessageFactory(type, settings, client, log, handleSelf = false) {
+/**
+ * Makes a message-sending handler for an event.
+ * @param {string} type The type of event message being handled.
+ * @param {SettingsManager} settings The settings to retrieve the channel's event message from.
+ * @param {Object} client The client.
+ * @param {*} log The logger for debug.
+ * @param {boolean} handleSelf Whether to handle the event message for events caused by the client.
+ * @returns {Function}
+ */
+function eventMessageFactory(type = "event", settings, client, log, handleSelf = false) {
 	return (channel, user) => {
 		if (user.nickname === client.nickname && !handleSelf) return;
 
