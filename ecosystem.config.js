2c220e972c540350ffe3968c3884f8a48643fd45
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-12-10 10:05:53 -0500

    Add environments to ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,16 @@
+/* eslint camelcase: off  */
+
 module.exports = {
 	apps: [{
 		autorestart: true,
+		env: {
+			DEBUG: "",
+			NODE_ENV: "production",
+		},
+		env_development: {
+			DEBUG: "snooful:*",
+			NODE_ENV: "development",
+		},
 		instances: 1,
 		name: "Snooful",
 		script: "./src/index.js",
