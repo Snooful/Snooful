fc2b6f87bceaa59ffcb42bfc8ebdd2530fef1161
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-02-06 17:06:17 -0500

    Create event-message-setter.js

---
+++ src/utils/event-message-setter.js
@@ -0,0 +1,39 @@
+/**
+ * Creates a setter command for event messages.
+ */
+module.exports = (command, opts = {}) => {
+	const options = {
+		longDescription: "Sets the message sent when an event occurs.",
+		msgType: "event_message",
+		storageKey: "event_message",
+		...opts,
+	};
+
+	return {
+		arguments: [{
+			description: "The new message.",
+			key: "event-message",
+			type: "string",
+		}],
+		description: "Sets the event message.",
+		handler: args => {
+			const oldMsg = args.settings.get(options.storageKey);
+			if (args.eventMessage) {
+				if (oldMsg === args.eventMessage) {
+					args.send(args.localize(options.msgType + "_update_no_change"));
+				} else {
+					args.settings.set(options.storageKey, args.eventMessage);
+					args.send(args.localize(options.msgType + "_update"));
+				}
+			} else if (oldMsg === undefined) {
+				args.send(args.localize(options.msgType + "_clear_no_change"));
+			} else {
+				args.settings.clear(options.storageKey);
+				args.send(args.localize(options.msgType + "_clear"));
+			}
+		},
+		longDescription: options.longDescription + " {USER} is replaced with the user's name.",
+		name: command,
+		...options.command,
+	};
+};
