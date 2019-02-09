0072f3ed55329d2d8f840c0ff7fa78e0e5c4bf73
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-02-09 09:50:04 -0500

    Add createrole command

---
+++ src/commands/permissions/createrole.js
@@ -0,0 +1,32 @@
+module.exports = {
+	aliases: [
+		"addrole",
+		"newrole",
+	],
+	arguments: [{
+		key: "role",
+		type: "string",
+	}],
+	handler: args => {
+		const roleName = args.role.replace(/[^a-z]+/g, "");
+
+		if (args.settings.get("roles") === undefined) {
+			args.settings.set("roles", {});
+		}
+
+		const roles = args.settings.get("roles");
+
+		if (roles[roleName]) {
+			return args.send(args.localize("role_already_exists"));
+		}
+
+		roles[roleName] = {
+			perms: [],
+			users: [],
+		};
+		args.settings.set("roles", roles);
+
+		args.send(args.localize("role_created"));
+	},
+	name: "createrole",
+};
