4fddbeb85a5465b8e9fbb9523df741a54a741ac2
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-02-09 18:26:55 -0500

    Add giveperm command

---
+++ src/commands/permissions/giveperm.js
@@ -0,0 +1,35 @@
+const roleNameify = require("./../../utils/role-name.js");
+const { validate } = require("@snooful/periwinkle-permissions");
+
+module.exports = {
+	aliases: [
+		"givepermission",
+		"grantperm",
+		"grantpermission",
+	],
+	arguments: [{
+		key: "role",
+		type: "string",
+	}, {
+		key: "permission",
+		type: "string",
+	}],
+	handler: args => {
+		if (!validate(args.permission)) {
+			return args.send(args.localize("invalid_permission"));
+		}
+
+		const roles = args.settings.get("roles");
+		const roleName = roleNameify(args.role);
+
+		if (!roles[roleName]) {
+			return args.send(args.localize("nonexistent_role"));
+		} else if (roles[roleName].perms.includes(args.permission)) {
+			return args.send(args.localize("permission_already_granted"));
+		}
+
+		roles[roleName].perms.push(args.permission);
+		args.send(args.localize("permission_granted"));
+	},
+	name: "giveperm",
+};
