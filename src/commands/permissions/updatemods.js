6af07a77821e6974df9e132af95a04186bbb1380
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-02-09 18:14:11 -0500

    Fix updatemods command not saving mod list

--- src/commands/permissions/updatemods.js
+++ src/commands/permissions/updatemods.js
@@ -8,8 +8,8 @@ module.exports = {
 		}
 
 		const sr = args.chData.subreddit.name;
-		args.reddit.getSubreddit(sr).getModerators().then(mod => {
-			console.log(mod);
+		args.reddit.getSubreddit(sr).getModerators().then(mods => {
+			args.settings.set("mods", mods.map(mod => mod.name));
 			args.send(args.localize("mod_update_success"));
 		}).catch(() => {
 			args.send(args.localize("mod_update_failure"));
