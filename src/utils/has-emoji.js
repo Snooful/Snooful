2bda9d4d64ca947dd4b0933fe162fbc3258ae8b2
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-02-10 10:14:33 -0500

    Document the has emoji utility

--- src/utils/has-emoji.js
+++ src/utils/has-emoji.js
@@ -1,5 +1,10 @@
 const emoji = require("node-emoji");
 
+/**
+ * Checks whether text includes emoji.
+ * @param {string} text The text to check for emoji.
+ * @returns {boolean} Whether the given text includes emoji.
+ */
 function hasEmoji(text) {
 	return text !== emoji.unemojify(text);
 }
