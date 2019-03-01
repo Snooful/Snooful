0d63b34b4345cae7d48793700cc6ab80b82b2e0f
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-02-28 19:42:25 -0500

    Add error messages to karma command

--- src/commands/util/karma.js
+++ src/commands/util/karma.js
@@ -7,16 +7,23 @@ module.exports = {
 	}],
 	category: "util",
 	describe: "Gets a user's karma.",
-	handler: async args => {
-		const user = await args.reddit.getUser(args.user).fetch();
-
-		const totalKarma = user.link_karma + user.comment_karma;
-		args.send(args.localize("karma", {
-			comment: user.comment_karma.toLocaleString(),
-			link: user.link_karma.toLocaleString(),
-			name: user.name,
-			total: totalKarma.toLocaleString(),
-		}));
+	handler: args => {
+		args.reddit.getUser(args.user).fetch().then(user => {
+			const totalKarma = user.link_karma + user.comment_karma;
+			args.send(args.localize("karma", {
+				comment: user.comment_karma.toLocaleString(),
+				link: user.link_karma.toLocaleString(),
+				name: user.name,
+				total: totalKarma.toLocaleString(),
+			}));
+		}).catch(error => {
+			// There has to be a better way...
+			if (error.error.error === 404) {
+				args.send(args.localize("karma_user_not_found"));
+			} else {
+				args.send(args.localize("karma_error"));
+			}
+		});
 	},
 	name: "karma",
 };
