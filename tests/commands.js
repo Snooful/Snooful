852e0b84b13da99d526f088ffce72d32d422c8fc
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-03-03 12:47:44 -0500

    Add tests for command descriptions

--- tests/commands.js
+++ tests/commands.js
@@ -15,4 +15,20 @@ describe("commands", () => {
 			assert.isString(cmd.category, `${cmd.name} does not have a category`);
 		});
 	});
+	it("have short descriptions", () => {
+		registry.forEach(cmd => {
+			assert.isString(cmd.description, `${cmd.name} does not have a description`);
+
+			// Length
+			assert(cmd.description.length < 50, `${cmd.name}'s description is too long`);
+			assert(cmd.description.length > 8, `${cmd.name}'s description is too short`);
+
+			// Capital letter
+			const firstLetter = cmd.description[0];
+			assert.strictEqual(firstLetter.toUpperCase(), firstLetter, `${cmd.name}'s description doesn't start with a capital letter`);
+
+			// Period at end
+			assert(cmd.description.endsWith("."), `${cmd.name}'s description doesn't end with a period`);
+		});
+	});
 });
