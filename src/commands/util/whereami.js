65fe7375309141ed4dcab34ec786666a999aedaf
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-07-03 21:27:10 -0400

    Add different messages for the whereami command in group and direct chats

--- src/commands/util/whereami.js
+++ src/commands/util/whereami.js
@@ -1,6 +1,17 @@
+const channelByType = {
+	default: args => args.channel.name,
+	direct: args => args.localize("whereami_direct", args.channel.inviter.nickname),
+	group: args => args.localize("whereami_group", args.channel.name),
+};
+
 module.exports = {
 	category: "util",
 	description: "Tells you where you are.",
-	handler: args => args.send(args.localize("whereami", args.channel.name)),
+	handler: args => {
+		const type = args.channel.customType;
+		const place = channelByType[type] ? channelByType[type](args) : channelByType.default(args);
+
+		args.send(args.localize("whereami", place));
+	},
 	name: "whereami",
 };
