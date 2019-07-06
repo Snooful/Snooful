ce57acf38f82cd4331cea9e28fb586c82d521f5b
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-07-06 00:12:21 -0400

    Fix old roles displaying their priority as undefined

--- src/commands/permissions/roles.js
+++ src/commands/permissions/roles.js
@@ -1,7 +1,7 @@
 const paginate = require("./../../utils/paginate.js");
 module.exports = paginate("roles", args => {
 	return Object.entries(args.settings.get("roles")).map(([ name, role ]) => {
-		const ending = " " + args.localize("role_listing_priority", role.priority);
+		const ending = " " + args.localize("role_listing_priority", typeof role.priority === "number" ? role.priority : 0);
 
 		return name + (name !== "user" ? ending : "");
 	});
