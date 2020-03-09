8e9a190aab06e42a3a79eb05fcf5cc3ac2886af6
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-03-09 12:14:42 -0400

    Localize the flip command

--- src/commands/chance/flip.js
+++ src/commands/chance/flip.js
@@ -1,17 +1,28 @@
 const chance = require("chance").Chance();
 
-module.exports = {
-	alias: "coin",
-	category: "chance",
-	description: "Flips a coin.",
-	handler: args => args.send(`The coin landed on ${chance.weighted([
+/**
+ * Gets the side of a coin flip.
+ * @returns {string} The side the coin landed on.
+ */
+function getCoinSide() {
+	return chance.weighted([
 		"heads",
 		"tails",
-		"its side",
+		"side",
 	], [
 		25,
 		25,
 		1,
-	])}!`),
+	]);
+}
+
+module.exports = {
+	alias: "coin",
+	category: "chance",
+	description: "Flips a coin.",
+	handler: args => {
+		const sideKey = "coin_flip_side_" + getCoinSide();
+		args.send(args.localize("coin_flip_result", args.localize(sideKey)));
+	},
 	name: "flip",
 };
