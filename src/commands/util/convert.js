9ec355e729e7dedbb57b2d8f1d0b7e42e0e0eb88
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-03-24 13:04:02 -0400

    Allow automatically selecting the best unit to convert to

--- src/commands/util/convert.js
+++ src/commands/util/convert.js
@@ -1,5 +1,28 @@
 const convert = require("convert-units");
 
+/**
+ * Converts a value to a given unit, or the best unit if unspecified.
+ * @param {number} value The value to convert.
+ * @param {string} from The unit to convert from.
+ * @param {string} [to] The unit to convert to.
+ * @returns {number} The converted value.
+ */
+function convertToUnitOrBest(value, from, to) {
+	const fromConvert = convert(value).from(from);
+	if (to) {
+		return [
+			fromConvert.to(to),
+			to,
+		];
+	} else {
+		const best = fromConvert.toBest();
+		return [
+			best.val,
+			best.unit,
+		];
+	}
+}
+
 module.exports = {
 	arguments: [{
 		description: "The value to convert.",
@@ -12,21 +35,24 @@ module.exports = {
 		required: true,
 		type: "string",
 	}, {
-		description: "The unit to convert to.",
+		description: "The unit to convert to. If unspecified, picks the best unit.",
 		key: "to",
-		required: true,
 		type: "string",
 	}],
 	category: "util",
 	description: "Converts between units.",
 	handler: args => {
 		if (args.from === args.to) {
-			return args.send("You cannot convert to the same unit.");
+			return args.send(args.localize("conversion_units_same"));
 		}
 
 		try {
-			const result = convert(args.value).from(args.from).to(args.to);
-			args.send(args.localize("conversion_result", args.value, args.from, result, args.to));
+			const [result, unit] = convertToUnitOrBest(args.value, args.from, args.to);
+
+			if (args.from === unit) {
+				return args.send(args.localize("conversion_unit_already_best"));
+			}
+			args.send(args.localize("conversion_result", args.value, args.from, result, unit));
 		} catch (error) {
 			if (error.message.startsWith("Unsupported unit")) {
 				return args.send(args.localize("conversion_unit_unsupported"));
