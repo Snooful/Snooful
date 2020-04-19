be8dd73512342f3101a9896cc265961bc9c16478
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-04-19 03:47:43 -0400

    Fix userperms command not flattening grouped permissions

--- src/commands/permissions/userperms.js
+++ src/commands/permissions/userperms.js
@@ -1,5 +1,5 @@
 const paginate = require("./../../utils/paginate.js");
-module.exports = paginate("userperms", args => args.perms, {
+module.exports = paginate("userperms", args => args.perms.flat(), {
 	command: {
 		category: "permissions",
 		description: "Lists the permissions a user has.",
