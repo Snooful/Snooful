3f79703f3f559713d35f9ea67f184ba95ffa25e6
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-06-11 17:51:05 -0400

    Add error messages to the rules command

--- src/commands/rules/rules.js
+++ src/commands/rules/rules.js
@@ -1,21 +1,26 @@
 const paginate = require("../../utils/paginate.js");
+const getSubredditErrorKey = require("../../utils/subreddit-error-key.js");
 
 module.exports = paginate("rules", async args => {
 	if (!args.chData.subreddit) {
-		return [];
+		return new Error(args.localize("rules_not_subreddit"));
 	}
 
 	const subredditName = args.chData.subreddit.name;
-	const subreddit = args.reddit.getSubreddit(subredditName);
 
-	const rules = await subreddit.getRules().catch(() => {
-		return {
-			rules: [],
-		};
-	});
-	return rules.rules.map(rule => {
-		return rule.short_name;
-	});
+	try {
+		const subreddit = args.reddit.getSubreddit(subredditName);
+		const { rules } = await subreddit.getRules();
+
+		return rules.map(rule => {
+			return rule.short_name;
+		});
+	} catch (error) {
+		if (error.response && error.response.body) {
+			return new Error(args.localize(getSubredditErrorKey("rules", error.response.body)));
+		}
+		return new Error(args.localize("rules_generic_error"));
+	}
 }, {
 	command: {
 		category: "rules",
